4ccf4ace17e1c0164b2c6ff74de03ad421dd808d
Lucas Scazzariello <lucas.scazza@gmail.com>
2021-02-16 23:27:45 -0300

    fixes

--- src/views/Experience/Juegos/Juegos.vue
+++ src/views/Experience/Juegos/Juegos.vue
@@ -8,7 +8,7 @@
       <img class="img-fluid" src="@/assets/img/juegos/logo-pixel.svg" alt="Rastro juegos">
     </div>
     <div class="juegos__text">
-      <h2 class="text-2">En la mochila, tenés un USB. La contraseña para abrirlo es:</h2>
+      <h2 class="text-2">En la mochila, tenes un USB. La password para abrirlo es:</h2>
       <h2 class="contrasena">Alpheido</h2>
     </div>
     <div class="juegos__btn">
@@ -19,21 +19,29 @@
             x-large 
             ripple 
             color="yellow" 
-            @click="nextStep('webapps')" 
+            @click="dialogConfirmation = true" 
             v-on="on">
             Seguir
           </v-btn>
         </template>
-        <span class="d-flex text-center">¿Te divertiste {{user.name}}? <br> Bueno es hora de seguir</span>
+        <span class="d-flex text-center">¿Te divertiste, {{user.name}}? <br> Bueno es hora de seguir</span>
       </v-tooltip>
     </div>
+    <dialog-confirm 
+      :active.sync="dialogConfirmation"
+      className="dialog-juegos"
+      title="Ya terminaste con los juegos?" 
+      content="Si lo hiciste podes continuar"
+      confirm-text="Continuar" 
+      @confirm="nextStep('webapps')">
+    </dialog-confirm>
   </div>
 </template>
-
 <script>
 
 import { mapState } from 'vuex';
 import helperApp from '@/mixins/helperApp';
+import DialogConfirm from '@/components/Dialogs/DialogConfirm';
 export default {
   name: 'ExpecienceJuegos',
   metaInfo() {
@@ -42,10 +50,13 @@ export default {
     }
   },
   mixins: [helperApp],
+  components: {
+    'dialog-confirm': DialogConfirm
+  },
   data() {
     return {
       loading: true,
-      code: ''
+      dialogConfirmation: false
     };
   },
   computed: {
