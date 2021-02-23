054a745d4e62237db6d9c9f6f681692bf287fc5e
Lucas Scazzariello <lucas.scazza@gmail.com>
2021-02-22 21:20:40 -0300

    main

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
-Vue.http.options.root = process.env.VUE_APP_SERVER
+Vue.http.options.root = "http//api:3000"
 
 // Config toastr alert
 toastr.options = {
